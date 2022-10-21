a4dd3f80d1f35772bd1fd615ce9fc34446a7ee0b
lagiewnik <lagiewnik@gmail.com>
2022-10-21 21:00:45 +0200

    retry ability

---
+++ cypress/e2e/TC03_retry-ability.cy.js
@@ -0,0 +1,13 @@
+describe('Retry-Ability Session', function(){
+    beforeEach(function () {
+        cy.visit("https://todomvc.com/examples/vue/");
+    
+        cy.get('.new-todo')
+            .type("todo A{enter}")
+            .type("todo B{enter}")
+    })
+
+    it('Should have 2 list elements', function(){
+        cy.get('.todo-list li').should('have.length',2)
+    })
+})
