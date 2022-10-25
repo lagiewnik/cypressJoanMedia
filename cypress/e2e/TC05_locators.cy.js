844a558adf9cf425107e6f294e61958ae6a2d797
ziom <lagiewnik@gmail.com>
2022-10-25 20:57:48 +0200

    locators ready

--- cypress/e2e/TC05_locators.cy.js
+++ cypress/e2e/TC05_locators.cy.js
@@ -8,10 +8,34 @@ describe("Locators demo", ()=>{
   
     it('should redirect to /inventory.html after login', () => {
         cy.get('#user-name').type(username);
-        cy.get('[data-test="password"]').type(password);
+        cy.get('input#password').type(password);
         cy.get('[data-test="login-button"]').click();
 
         cy.location('pathname').should('eq','/inventory.html')
 
+    });
+
+    it('EQ, FIRST, LAST method', () => {
+        cy.get('input').first().type(username);
+        cy.get('input').eq(1).type(password);
+        cy.get('input').last().click();
+
+    });
+
+    it('FILTER Method', () => {
+        cy.get('input').filter('[type="text"]').type(username);
+        cy.get('input').filter('[type="password"]').type(password);
+        cy.get('input').filter('[type="submit"]').click();
+
+    });
+
+    it('FIND method', () => {
+        cy.get('form').find('input').eq(0).type(username);
+        cy.get('form').find('input').eq(1).type(password);
+        cy.get('form').find('input').eq(2).click();
+    });
+
+    it('PARENT Method', () => {
+        cy.get('form').parent().should('have.class','login-box');
     });
 })
