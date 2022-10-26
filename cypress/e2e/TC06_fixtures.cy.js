d55b1e828e5bff9c685b0be77edefc27b104f6a5
ziom <lagiewnik@gmail.com>
2022-10-26 14:16:15 +0200

    custom commands handling

--- cypress/e2e/TC06_fixtures.cy.js
+++ cypress/e2e/TC06_fixtures.cy.js
@@ -4,38 +4,38 @@ describe("Fixtures demo", function() {
     beforeEach(function(){
         cy.visit('https://saucedemo.com')
 
-        cy.fixture("saucedemo/sauceCredentials").then(credencials =>{
-            this.credencials = credencials;
+        cy.fixture("saucedemo/sauceCredentials").then(credentials =>{
+            this.credentials = credentials;
         })
     })
 
     it('Standard user login from fixtures', function() {
-        cy.get('[data-test="username"]').type(this.credencials.standardUsername);
-        cy.get('[data-test="password"]').type(this.credencials.systemPassword);
+        cy.get('[data-test="username"]').type(this.credentials.standardUsername);
+        cy.get('[data-test="password"]').type(this.credentials.systemPassword);
         cy.get('[data-test="login-button"]').click();
 
         cy.get('.title').should('contain.text','Products');
     });
 
     it('Incorrect  user login from fixtures', function() {
-        cy.get('[data-test="username"]').type(this.credencials.dummyUsername);
-        cy.get('[data-test="password"]').type(this.credencials.systemPassword);
+        cy.get('[data-test="username"]').type(this.credentials.dummyUsername);
+        cy.get('[data-test="password"]').type(this.credentials.systemPassword);
         cy.get('[data-test="login-button"]').click();
 
         cy.get('[data-test="error"]').should('contain.text','Epic sadface: Username and password do not match any user in this service');
     });
   
     it('Incorrect  password login from fixtures', function() {
-        cy.get('[data-test="username"]').type(this.credencials.standardUsername);
-        cy.get('[data-test="password"]').type(this.credencials.dummyPassword);
+        cy.get('[data-test="username"]').type(this.credentials.standardUsername);
+        cy.get('[data-test="password"]').type(this.credentials.dummyPassword);
         cy.get('[data-test="login-button"]').click();
 
         cy.get('[data-test="error"]').should('contain.text','Epic sadface: Username and password do not match any user in this service');
     });
     
     it('Incorrect  password login from fixtures', function() {
-        cy.get('[data-test="username"]').type(this.credencials.lockedUsername);
-        cy.get('[data-test="password"]').type(this.credencials.systemPassword);
+        cy.get('[data-test="username"]').type(this.credentials.lockedUsername);
+        cy.get('[data-test="password"]').type(this.credentials.systemPassword);
         cy.get('[data-test="login-button"]').click();
 
         cy.get('[data-test="error"]').should('contain.text','Epic sadface: Sorry, this user has been locked out.');
