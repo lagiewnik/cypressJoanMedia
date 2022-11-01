8206a4d3334be5fee3d2d199d801bb69e8aebc0e
lagiewnik <lagiewnik@gmail.com>
2022-11-01 13:15:03 +0100

    API POST PATCH DELETE

---
+++ cypress/e2e/API/TC10_API-POST_PUT_DELETE.cy.js
@@ -0,0 +1,56 @@
+context('POST PUT DELETE - API demo', () => {
+    const endpoint = 'http://localhost:3000/todos'
+
+    const todoObject = {
+        "title": "Adding new todos",
+        "completed": false,
+        "id": 999
+    }
+
+    const todoObjectPatch = {
+        "title": "Adding new todos",
+        "completed": true,
+        "id": 999
+    }
+
+    const addTodo = todoObject => {
+        cy.request("POST", endpoint, todoObject)
+    }
+
+    const updateTodo = todoObjectPatch => {
+        cy.request("PATCH", `${endpoint}/${todoObjectPatch.id}`, todoObjectPatch)
+    }
+
+    const deleteTodo = todoToDelete => {
+        cy.request("DELETE", `${endpoint}/${todoToDelete.id}`, todoToDelete)
+    }
+
+    it('Add a todo', function(){
+        addTodo(todoObject);
+
+        cy.request('GET',`${endpoint}/${todoObject.id}`)
+            .its('body')
+            .should('deep.equal',todoObject)
+    
+    })
+
+    it('Update a todo', () => {
+        updateTodo(todoObjectPatch)
+
+        cy.request('GET',`${endpoint}/${todoObjectPatch.id}`)
+            .its('body')
+            .should('deep.equal',todoObjectPatch)
+
+    });
+
+    it('Delete todo', () => {
+        deleteTodo(todoObject);
+
+        cy.request('GET',endpoint)
+            .its('body')
+            .should('have.length',2)
+
+    });
+
+});
+
