f3cfad337501f45f88cc8e9d4cd94de7d591e93a
ziom <lagiewnik@gmail.com>
2022-11-10 11:55:27 +0100

    cypress-tags

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,7 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-import 'cypress-mochawesome-reporter/register';
+//import 'cypress-mochawesome-reporter/register';
 
 
 Cypress.on('uncaught:exception', (err, runnable) => {
